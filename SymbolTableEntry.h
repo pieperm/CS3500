5fefaf5d49dcf2e485f3648fbfe7d5e25c04d1df
Michael Pieper <michaelepieper@gmail.com>
2020-03-18 22:07:19 -0500

    Move TYPE_INFO to SymbolTableEntry.h, fix default constructor

--- SymbolTableEntry.h
+++ SymbolTableEntry.h
@@ -2,39 +2,43 @@
 #define SYMBOL_TABLE_ENTRY_H
 
 #include <string>
+
 using namespace std;
 
 #define UNDEFINED  -1
 
-class SYMBOL_TABLE_ENTRY 
-{
+typedef struct {
+    int type;
+    int numParams;
+    int returnType;
+} TYPE_INFO;
+
+class SYMBOL_TABLE_ENTRY {
 private:
-  // Member variables
-  string name;
-  int typeCode;  
-	TYPE_INFO typeInfo;
+    // Member variables
+    string name;
+    TYPE_INFO typeInfo;
 
 public:
-  // Constructors
-  SYMBOL_TABLE_ENTRY( ) 
-	{
-		name = ""; 
-		typeInfo.type = UNDEFINED; 
-		typeInfo.numParams = theType.numParams;
-		typeInfo.returnType = theType.returnType;
-	}
+    // Constructors
+    SYMBOL_TABLE_ENTRY() {
+        name = "";
+        typeInfo.type = UNDEFINED;
+        typeInfo.numParams = UNDEFINED;
+        typeInfo.returnType = UNDEFINED;
+    }
 
-  SYMBOL_TABLE_ENTRY(const string theName, const TYPE_INFO theType) 
-  {
-    name = theName;
-    typeInfo.type = theType.type;
-		typeInfo.numParams = theType.numParams;
-		typeInfo.returnType = theType.returnType;
-  }
+    SYMBOL_TABLE_ENTRY(const string theName, const TYPE_INFO theType) {
+        name = theName;
+        typeInfo.type = theType.type;
+        typeInfo.numParams = theType.numParams;
+        typeInfo.returnType = theType.returnType;
+    }
 
-  // Accessors
-  string getName() const { return name; }
-  TYPE_INFO getTypeInfo() const { return typeInfo; }
+    // Accessors
+    string getName() const { return name; }
+
+    TYPE_INFO getTypeInfo() const { return typeInfo; }
 };
 
 #endif  // SYMBOL_TABLE_ENTRY_H
