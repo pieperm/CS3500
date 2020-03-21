8f318d3f6194e64d5f3cc5b4dabf0db6acb2aebf
Nick Ekstrom <nekstrom98@gmail.com>
2020-03-21 13:50:25 -0500

    Revert "Separate findEntry and getEntry into two independent functions (avoids unnecessary usage of pointers)"
    This reverts commit be6ed240d3efa5a52db73cd0f2b87c185cdb05e3.

--- SymbolTable.h
+++ SymbolTable.h
@@ -17,10 +17,17 @@
 #include "SymbolTableEntry.h"
 using namespace std;
 
+typedef struct {
+    int type;
+    int numParams;
+    int returnType;
+} TYPE_INFO;
+
 class SYMBOL_TABLE 
 {
 private:
   std::map<string, SYMBOL_TABLE_ENTRY> hashTable;
+	TYPE_INFO typeInfo;
 
 public:
   //Constructor
@@ -43,18 +50,12 @@ public:
   // If a SYMBOL_TABLE_ENTRY with name theName is
   // found in this symbol table, then return true;
   // otherwise, return false.
-  bool findEntry(string theName)
+  TYPE_INFO findEntry(string theName) 
   {
     map<string, SYMBOL_TABLE_ENTRY>::iterator itr;
     if ((itr = hashTable.find(theName)) == hashTable.end())
-      return(false);
-    else return(true);
-  }
-
-  // assumes that entry with theName exists
-  SYMBOL_TABLE_ENTRY getEntry(string theName)
-  {
-      return hashTable.at(theName);
+      return(UNDEFINED);
+    else return(typeInfo);
   }
 
 };
