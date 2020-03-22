afb92837850019b462e97995d094fa8473d9bd54
Michael Pieper <michaelepieper@gmail.com>
2020-03-21 19:04:45 -0500

    Fix typo, reset file each run to avoid appending

--- compare_output.sh
+++ compare_output.sh
@@ -22,6 +22,7 @@ g++ ekstromn.tab.c -o mfpl_parser
 
 test_files=`ls ./sample_input`
 diff_files=0
+echo > comparison.txt
 
 for file in $test_files; do
 	./mfpl_parser.exe < ./sample_input/$file > ./my_output/$file.out
@@ -32,13 +33,13 @@ for file in $test_files; do
 					
 	if [ $diff_lines == 0 ]
 	then
-		echo $file matches. >> comparision.txt
+		echo $file matches. >> comparison.txt
 	else
 		diff_files=$((diff_files+1))
-		echo $file does not match. There are $diff_lines differences. >> comparision.txt
+		echo $file does not match. There are $diff_lines differences. >> comparison.txt
 	fi
 done
 
-echo >> comparision.txt
-echo "Number of different input files: $diff_files" >> comparision.txt
-echo >> comparision.txt
+echo >> comparison.txt
+echo "Number of different input files: $diff_files" >> comparison.txt
+echo >> comparison.txt
